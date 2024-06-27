f519c4b83bbd46f44776672a67539a5b12e4f20b
RaKalik963 <andrejmahot646@gmail.com>
2024-06-27 20:26:31 +0300

    dfg

--- ur18_27.06_str2/main.cpp
+++ ur18_27.06_str2/main.cpp
@@ -2,22 +2,21 @@
 using namespace std;
 
 
-char* mystrcpy(char* str1, const char* str2)
+char* mystrcpy(char* str1, const char* str2) 
 {
-	for (size_t i = 0; str2[i] != '\0'; i++)
-	{
-		str1[i] == str2[i];
-		
-	}
-	return str1;
+    char* res = str1;
+
+    while ((*str1++ = *str2++) != '\0');
+    cout << str1 << endl;
+    return res;
 }
 
 int main()
 {
     const char str2[] = "hello world!";
     char str1;
-	cout << mystrcpy(str1, str2) << endl;
-
+	
+    cout << " " << mystrcpy(str1, str2) << endl;
     system("pause");
     return 0;
 }
