3eee323e8684be1e2dd6f51c8a535f348cce4e23
RaKalik963 <andrejmahot646@gmail.com>
2024-06-27 20:44:26 +0300

    qeqe

--- ur18_27.06_str2/main.cpp
+++ ur18_27.06_str2/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 
-
+char* mystrcat(char* str1, const char* str2)
+{
+	for (size_t i = 0; str1[i] != '\0'; i++)
+	{
+		str1[i] == str2[i];
+		cout << str1 << endl;
+	}
+	return str1;
+}
 
 
 int main()
 {
-
+	char str[] = "hello";
+	const char str2[] = " world";
+	mystrcat(str1, str2);
 	system("pause");
 	return 0;
 }
