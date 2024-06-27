e56c39d9759111152c07a648b014cc3dd855eeb9
RaKalik963 <andrejmahot646@gmail.com>
2024-06-27 20:00:50 +0300

    3

--- ur18_27.06_str2/main.cpp
+++ ur18_27.06_str2/main.cpp
@@ -2,11 +2,21 @@
 using namespace std;
 
 
-
+char* mystrchr(char* str, char s)
+{
+	for (size_t i = 0; str[i] != '\0'; i++)
+	{
+		if (str == ' ')
+		{
+			
+		}
+	}
+}
 
 int main()
 {
-
+	char str[] = "hello worlds";
+	char s[] = "s";
 	system("pause");
 	return 0;
 }
