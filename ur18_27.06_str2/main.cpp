51bbbfc62d9f6bb352721d8b79c9ecb8279a427b
RaKalik963 <andrejmahot646@gmail.com>
2024-07-01 00:24:12 +0300

    dz-ex6

--- ur18_27.06_str2/main.cpp
+++ ur18_27.06_str2/main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 using namespace std;
 
+char* mystrrev(char* str) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
 
+    int start = 0;
+    int end = length - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
 
+    return str;
+}
 
 int main()
 {
-
+    char str[] = "Hello World";
+    cout << "Reversed string: " << mystrrev(str) << endl;
 	system("pause");
 	return 0;
 }
