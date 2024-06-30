08bebc8fa1907382a1bfeee58d3e214f8ea0810c
RaKalik963 <andrejmahot646@gmail.com>
2024-06-30 21:38:41 +0300

    lab-ex3

--- ur18_27.06_str2/main.cpp
+++ ur18_27.06_str2/main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
 
-char* mystrcat(char* str1, const char* str2)
-{
-	for (size_t i = 0; str1[i] != '\0'; i++)
-	{
-		str1[i] == str2[i];
-		cout << str1 << endl;
-	}
-	return str1;
+char* mystrcat(char* str1, const char* str2) {
+    int i = 0;
+    int j = 0;
+
+    while (str1[i] != '\0') {
+        ++i;
+    }
+
+    while (str2[j] != '\0') {
+        str1[i] = str2[j];
+        ++i;
+        ++j;
+    }
+    str1[i] = '\0';
+    return str1;
 }
 
 
 int main()
 {
-	char str[] = "hello";
-	const char str2[] = " world";
+	char str1[100] = "hello";
+	char str2[] = " world";
 	mystrcat(str1, str2);
+    cout << str1 << endl;
 	system("pause");
 	return 0;
 }
