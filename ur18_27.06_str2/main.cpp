03d0f78d18db361f3ed167d4e7f154e82905b8c8
RaKalik963 <andrejmahot646@gmail.com>
2024-07-01 00:20:57 +0300

    dz-ex4

--- ur18_27.06_str2/main.cpp
+++ ur18_27.06_str2/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
 
-
-
+char* Uppercase(char* str1) {
+    char* p = str1;
+    while (*p) {
+        if (*p >= 'a' && *p <= 'z') {
+            *p -= 32;
+        }
+        p++;
+    }
+    return str1;
+}
 
 int main()
 {
-
+    char str1[] = "Hello World";
+    cout << "Uppercase: " << Uppercase(str1) << endl;
 	system("pause");
 	return 0;
 }
