b1f24853b2e6bbe4b72691fd9a5cabb5d086665c
RaKalik963 <andrejmahot646@gmail.com>
2024-07-01 00:06:05 +0300

    dz1

--- ur18_27.06_str2/main.cpp
+++ ur18_27.06_str2/main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 using namespace std;
 
-
+int mystrcmp(const char* str1, const char* str2) {
+    while (*str1 && (*str1 == *str2)) {
+        str1++;
+        str2++;
+    }
+    if (*str1 > *str2) {
+        return 1;
+    }
+    else if (*str1 < *str2) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
 
 
 int main()
 {
+    char str1[200] = "the function searches for substring str2 in string str1.";
+    cout << "Original string: " << str1 << endl;
 
+    char str2[200];
+    cout << "Enter str2: ";
+    cin.getline(str2, 100);
+
+    int cmp_result = mystrcmp(str1, str2);
+    cout << "Comparison result: " << cmp_result << endl;
 	system("pause");
 	return 0;
 }
