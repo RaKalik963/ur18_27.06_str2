c87201337b1b32e867e607ad56a7748c05168a6f
RaKalik963 <andrejmahot646@gmail.com>
2024-07-01 00:22:11 +0300

    dz-ex5

--- ur18_27.06_str2/main.cpp
+++ ur18_27.06_str2/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
 
-
-
+char* Lowercase(char* str1) {
+    char* p = str1;
+    while (*p) {
+        if (*p >= 'A' && *p <= 'Z') {
+            *p += 32;
+        }
+        p++;
+    }
+    return str1;
+}
 
 int main()
 {
-
+	char str1[] = "HELLO WORLD";
+	cout << "Lowercase: " << Lowercase(str1) << endl;
 	system("pause");
 	return 0;
 }
