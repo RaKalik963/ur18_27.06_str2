44f71177189e7b7bc00433262d50ba71f97d7338
RaKalik963 <andrejmahot646@gmail.com>
2024-06-30 21:59:51 +0300

    lab-ex4

--- ur18_27.06_str2/main.cpp
+++ ur18_27.06_str2/main.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 using namespace std;
 
-
-char* mystrchr(char* str, char s)
-{
-	for (size_t i = 0; str[i] != '\0'; i++)
-	{
-		if (str == ' ')
-		{
-			
-		}
-	}
+char* mystrchr(char* str, char s) {
+    while (*str != '\0') {
+        if (*str == s) {
+            return str;
+        }
+        ++str;
+    }
+    return nullptr;
 }
 
-int main()
-{
-	char str[] = "hello worlds";
-	char s[] = "s";
-	system("pause");
-	return 0;
+int main() {
+    char str[100];
+    cout << "Enter str: ";
+    cin.getline(str, 100);
+
+    char s = 's'; 
+
+    char* pos = mystrchr(str, s);
+    if (pos) {
+        cout << "symbol '" << s << "' is found in str: " << pos << endl;
+    }
+    else {
+        cout << "symbol '" << s << "' is not found in str." << endl;
+    }
+
+    system("pause");
+    return 0;
 }
