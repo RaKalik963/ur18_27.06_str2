0e106e773593c7c88608af5533c8fc8018ff084f
RaKalik963 <andrejmahot646@gmail.com>
2024-07-01 00:12:00 +0300

    dz-ex2

--- ur18_27.06_str2/main.cpp
+++ ur18_27.06_str2/main.cpp
@@ -2,10 +2,31 @@
 using namespace std;
 
 
+int StringToNumber(char* str) {
+    int number = 0;
+    bool isNegative = false;
 
+    if (*str == '-') {
+        isNegative = true;
+        str++;
+    }
+
+    while (*str) {
+        number = number * 10 + (*str - '0');
+        str++;
+    }
+
+    return isNegative ? -number : number;
+}
 
 int main()
 {
+    char str[100];
+    cout << "Enter str2: ";
+    cin.getline(str, 100);
+
+    int num = StringToNumber(str);
+    cout << "String to number: " << num << endl;
 
 	system("pause");
 	return 0;
