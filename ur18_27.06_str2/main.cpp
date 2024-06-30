d922bc475f744e489ccb6fdccbf81ab08e609d6d
RaKalik963 <andrejmahot646@gmail.com>
2024-06-30 23:57:26 +0300

    lab-ex5

--- ur18_27.06_str2/main.cpp
+++ ur18_27.06_str2/main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 using namespace std;
 
+char* mystrstr(char* str1, const char* str2) {
+    if (*str2 == '\0') {
+        return str1;
+    }
 
+    for (char* p1 = str1; *p1 != '\0'; ++p1) {
+        char* p1_begin = p1;
+        const char* p2 = str2;
 
+        while (*p1_begin != '\0' && *p2 != '\0' && *p1_begin == *p2) {
+            ++p1_begin;
+            ++p2;
+        }
 
-int main()
-{
+        if (*p2 == '\0') {
+            return p1;
+        }
+    }
+
+    return nullptr;
+}
+
+int main() {
+    char str1[200] = " the function searches for substring str2 in string str1.";
+    cout << str1 << endl;
+
+    char str2[100];
+    cout << "Enter str2: ";
+    cin.getline(str2, 100);
+
+    char* result = mystrstr(str1, str2);
+    if (result) {
+        cout << "Found: " << endl;
+    }
+    else {
+        cout << "0" << endl;
+    }
 
 	system("pause");
 	return 0;
